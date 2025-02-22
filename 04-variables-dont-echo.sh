502540926b3e36ca00c2fe4c2dc203bddb0678d7
unknown <vishnu.madhanapalli541@gmail.com>
2025-02-22 16:17:43 +0530

    message

--- 04-variables-dont-echo.sh
+++ 04-variables-dont-echo.sh
@@ -3,4 +3,4 @@ echo "please enter the user name ::"
 read -s USERNAME
 echo "please enter the password ::" 
  read -s PASSWORD
-echo " username :: $USERNAME and password :: $PASSWORD"
+#echo " username :: $USERNAME and password :: $PASSWORD"
